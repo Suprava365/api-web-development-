f0742e25b56864513d467e99f23d136d919d89a7
Suprava365 <supravagiri365@gmail.com>
2025-06-13 16:04:29 +0545

    added index file

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require('./src/app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('✅ MongoDB connected');
+  app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+})
+.catch(err => console.error('❌ DB connection error:', err));
