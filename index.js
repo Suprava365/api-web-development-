c8396f95b418c4548493d4dac364e7ba36be8574
Suprava365 <supravagiri365@gmail.com>
2025-06-13 16:24:43 +0545

    modified index

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const app = require('./src/app');
-const mongoose = require('mongoose');
-require('dotenv').config();
+      
+
+const app       = require('./src/app');     
+const connectDB = require('./src/config/db'); 
+
+
+require('dotenv').config();    
+
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('✅ MongoDB connected');
-  app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
-})
-.catch(err => console.error('❌ DB connection error:', err));
+(async () => {
+  try {
+    await connectDB();                     
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error('Failed to start server:', err);
+    process.exit(1);                      
+  }
+})();
