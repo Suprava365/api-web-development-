766d3ee64da7447d78741b432c9941fb59172139
Suprava365 <supravagiti365@gmail.com>
2025-08-01 23:35:17 +0545

    some error

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -48,12 +48,12 @@ exports.getBookingById = async (req, res) => {
   }
 };
 
-exports.updateBooking = async (req, res) => {
+
+exports.deleteBooking = async (req, res) => {
   try {
-    const booking = await bookingService.updateBooking(req.params.bookingId, req.body);
-    res.json({ success: true, message: 'Booking updated', data: booking });
+    await bookingService.deleteBooking(req.params.bookingId);
+    res.json({ success: true, message: 'Booking deleted' });
   } catch (err) {
     res.status(400).json({ success: false, message: err.message });
   }
 };
-
