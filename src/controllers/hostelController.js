dad13cc32b50558041438f9fb86ad8df97610b74
Suprava365 <supravagiti365@gmail.com>
2025-08-01 23:38:43 +0545

    hostel update controller erro

--- src/controllers/hostelController.js
+++ src/controllers/hostelController.js
@@ -11,17 +11,17 @@ exports.createHostel = async (req, res) => {
   }
 };
 
-
-// UPDATE
-exports.updateHostel = async (req, res) => {
+// READ ALL
+exports.getAllHostels = async (req, res) => {
   try {
-    const updated = await Hostel.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!updated) return res.status(404).json({ message: 'Hostel not found' });
-    res.status(200).json({ message: 'Hostel updated', hostel: updated });
+    const hostels = await Hostel.find().sort({ createdAt: -1 });
+    res.status(200).json(hostels);
   } catch (err) {
-    res.status(500).json({ message: 'Error updating hostel', error: err.message });
+    res.status(500).json({ message: 'Error fetching hostels', error: err.message });
   }
 };
+
+
 
 // DELETE
 exports.deleteHostel = async (req, res) => {
