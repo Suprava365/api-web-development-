a8e3cdf7636e66219729f6f7ea1f3f1300f498d5
Suprava365 <supravagiti365@gmail.com>
2025-08-01 23:25:25 +0545

    handle change fixed

--- frontend/src/components/dashboard/HostelTable.jsx
+++ frontend/src/components/dashboard/HostelTable.jsx
@@ -53,6 +53,20 @@ export default function HostelTable() {
     fetchHostels();
   }, []);
 
+const handleChange = (e) => {
+  const { name, type, checked, value } = e.target;
+
+  if (type === "checkbox") {
+    setFormData((prev) => ({ ...prev, [name]: checked }));
+  } else if (type === "number") {
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value === "" ? "" : Number(value),
+    }));
+  } else {
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
+};
 
 
   const openAddModal = () => {
