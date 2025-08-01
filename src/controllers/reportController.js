5b847fa715b458e6ba41310e57adb0ee1839d368
Suprava365 <supravagiti365@gmail.com>
2025-08-01 23:41:38 +0545

    get feed and complaints

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -9,23 +9,6 @@ exports.getOccupancyRate = async (req, res) => {
   }
 };
 
-exports.getDueFees = async (req, res) => {
-  try {
-    const due = await reportService.getDueFees();
-    res.json({ success: true, data: due });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
-
-exports.getComplaintStats = async (req, res) => {
-  try {
-    const stats = await reportService.getComplaintStats();
-    res.json({ success: true, data: stats });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
 
 exports.getRoomAssignments = async (req, res) => {
   try {
