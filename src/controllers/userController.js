198d2130dff16e27b8d5c8a08b17a1f5b18ec433
Suprava365 <supravagiti365@gmail.com>
2025-08-01 23:36:43 +0545

    create user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,32 +1,17 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
-
-
-// CREATE new user
-exports.createUser = async (req, res) => {
+// GET all users
+exports.getAllUsers = async (req, res) => {
   try {
-    const { firstName, lastName, email, password, role, status } = req.body;
-    const existing = await User.findOne({ email });
-    if (existing) return res.status(400).json({ message: 'Email already exists' });
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = new User({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-      role,
-      status,
-    });
-
-    await newUser.save();
-    res.status(201).json({ message: 'User created successfully', user: newUser });
+    const users = await User.find().sort({ createdAt: -1 });
+    res.json(users);
   } catch (err) {
-    res.status(500).json({ message: 'Failed to create user' });
+    res.status(500).json({ message: 'Failed to fetch users' });
   }
 };
+
+
 
 // UPDATE user
 exports.updateUser = async (req, res) => {
